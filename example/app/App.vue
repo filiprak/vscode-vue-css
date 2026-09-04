f564fb72b1b20b93c99af6f83508370352660d1f
Filip Rak <filiprak@wp.pl>
2026-09-05 00:50:20 +0200

    Work in progress

--- example/app/App.vue
+++ example/app/App.vue
@@ -1,9 +1,10 @@
 <template>
   <!-- Type inside class="..." to get completions, Ctrl+click a name to jump to CSS -->
   <div class="app">
-    <div class="card card-title">Hello</div>
+    <div class="card card-title m-1">Hello</div>
   </div>
 </template>
 <script setup lang="ts">
+import "../css";
 import "./App.css";
 </script>
